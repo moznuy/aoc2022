4da81e8917b8a66c94c5d4757bf913bfd82f6312
Serhii Charykov <laammaar@gmail.com>
2022-12-06 18:20:11 +0200

    Day 6

---
+++ day6.py
@@ -0,0 +1,32 @@
+import collections
+
+import utils
+
+from itertools import islice
+
+
+# Python docs :)
+def sliding_window(iterable, n):
+    it = iter(iterable)
+    window = collections.deque(islice(it, n), maxlen=n)
+    if len(window) == n:
+        yield tuple(window)
+    for x in it:
+        window.append(x)
+        yield tuple(window)
+
+
+def solution(level):
+    group = 4 if level == 1 else 14
+
+    for line in utils.input_reader():
+        for i, window in enumerate(sliding_window(line, group)):
+            if len(set(window)) == group:
+                print(i + group)
+                break
+        else:
+            raise NotImplementedError
+
+
+if __name__ == '__main__':
+    solution(utils.get_level())
